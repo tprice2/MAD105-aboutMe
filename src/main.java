57ce9d9f9ae093b5cad47515c5b6a729f981793c
tprice2 <42660616+tprice2@users.noreply.github.com>
2020-01-23 20:02:12 -0600

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main1(String[] args)
+    {
+        System.out.println("Hi, my name is Trevor.");
+        System.out.println("I'm 24 years old.");
+        System.out.println("I work as an engineer and graduated from UofI in 2017.");
+        System.out.println("I like hockey!");
+    }
+}
